754bafc26f11ad44a5298e502ea9ac668fb188d5
Disha2901185 <dishakansal2410@gmail.com>
2026-05-24 13:32:21 +0530

    Fix routing structure in App component

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
               <Route path="/login" element={<Login />} />
             <Route path="/" element={<ProjectList />}>
               <Route path="/profile" element={<Profile />} />
+              
             </Route>
           </Routes>
         </BrowserRouter>
